f68f292dbfc1fc890048a0cc55646769c5aaf93e
TheNote <38653750+note3crafter@users.noreply.github.com>
2023-07-25 19:25:47 +0200

    Add files via upload

---
+++ src/TheNote/RandomNick/Main.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace TheNote\RandomNick;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerLoginEvent;
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+
+class Main extends PluginBase implements Listener {
+    /** @var array */
+    private $nicknames = [];
+
+    public function onEnable(): void {
+        $this->saveResource("config.yml");
+        $this->loadNicknamesFromConfig();
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    private function loadNicknamesFromConfig(): void {
+        $config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+        $this->nicknames = $config->get("nicknames", []);
+    }
+
+    public function onLogin(PlayerLoginEvent $event): void {
+        $player = $event->getPlayer();
+        $this->setRandomNickname($player);
+    }
+    private function setRandomNickname(Player $player): void
+    {
+        if (empty($this->nicknames)) {
+            $player->getName();
+            return;
+        }
+
+        $randomNickname = $this->nicknames[array_rand($this->nicknames)];
+        $player->setDisplayName($randomNickname);
+        $player->setNameTag($randomNickname);
+    }
+}
